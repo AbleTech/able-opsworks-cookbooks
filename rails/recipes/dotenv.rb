ebc0981e84a9000a830438ffd1b00c05022ddbfd
Marcus Baguley <marcus.baguley@gmail.com>
2015-07-09 11:57:21 +1200

    add config dir

--- rails/recipes/dotenv.rb
+++ rails/recipes/dotenv.rb
@@ -17,6 +17,14 @@ node[:deploy].each do |application, deploy|
 
   Chef::Log.info("Generating dotenv for app: #{application} with env: #{rails_env}...")
 
+  directory "#{deploy[:deploy_to]}/shared/config" do
+    group deploy[:group]
+    owner deploy[:user]
+    mode "0775"
+    action :create
+    recursive true
+  end
+
   open("#{deploy[:deploy_to]}/shared/config/.env", 'w') do |f|
     require 'yaml'
     deploy[:environment_variables].to_h.each do |name, value|
