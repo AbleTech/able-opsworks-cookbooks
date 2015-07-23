b29fee4572cd95f3a2695bf28e152096fb31d368
Marcus Baguley <marcus.baguley@gmail.com>
2015-07-23 13:56:45 +1200

    fix location of bin and options

--- sneakers_worker/recipes/template.rb
+++ sneakers_worker/recipes/template.rb
@@ -17,8 +17,8 @@ node[:deploy].each do |application, deploy|
       :dir => deploy[:deploy_to],
       :user => deploy[:user],
       :group => deploy[:group],
-      :start_command => deploy[:bin],
-      :sneakers_options => global[:options]
+      :start_command => global[:bin],
+      :sneakers_options => deploy[:sneakers_worker] && deploy[:sneakers_worker][:options]
     )
     only_if { deploy[:sneakers_worker] }
   end
